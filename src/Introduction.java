074eece9fd6140193cdde6dd75123d87c9251d98
rwangari <rwangari@rwanda.cmu.edu>
2018-08-02 12:54:48 +0200

    Added a java file to test the repository

---
+++ src/Introduction.java
@@ -0,0 +1,20 @@
+
+public class Introduction {
+	
+	String intro = "Introduction";
+  
+	public Introduction() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * A method to test our infrastructure
+	 * @return
+	 */
+	public String getIntroduction() {
+		
+		return intro;
+		
+	}
+ 
+}
