cc8f546451443664a74cc3ba3d66cea2d527092f
rwangari <rwangari@rwanda.cmu.edu>
2018-08-19 23:06:43 +0200

    Creating a branch for loading data from different sources

---
+++ src/LoadingData.java
@@ -0,0 +1,17 @@
+/**
+ * Video 2.1 : Loading data from different sources
+ * @author rwangari
+ *
+ */
+public class LoadingData {
+
+	public LoadingData() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
