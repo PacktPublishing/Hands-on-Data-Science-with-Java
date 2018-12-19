f15e27863a38cd7ad6c348b864a6fb5db831b678
rwangari <rwangari@rwanda.cmu.edu>
2018-12-19 21:14:01 +0200

    Created Section 4  and created a class for Linear Regression

---
+++ src/HousePricesAnalysis.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author rwangari
+ * Section 4
+ * Analyzing House data with Linear Regression
+ *
+ */
+
+public class HousePricesAnalysis {
+
+	public HousePricesAnalysis() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
