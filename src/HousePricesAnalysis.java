0f86541774dbda3ce771d1e14aefea43ecc3cc85
rwangari <rwangari@rwanda.cmu.edu>
2019-01-17 16:07:49 +0200

    Created new files for house analysis

--- src/HousePricesAnalysis.java
+++ src/HousePricesAnalysis.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+
+import tech.tablesaw.api.Table;
+
 /**
  * 
  * @author rwangari
@@ -9,12 +13,25 @@
 public class HousePricesAnalysis {
 
 	public HousePricesAnalysis() {
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub	
+	
+		
+	}
+	
+	public void cleanData (Table myTable) {
+		
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
+		//Loading the train dataset
+		Table HousePrices_Train  = Table.read().csv("DataSets/HousePrices.csv");
+		
+		
+		System.out.println("Printing the last  rows of the table");
+		Table tableTail = HousePrices_Train.last(5);
+		System.out.println(tableTail);
 	}
 
 }
