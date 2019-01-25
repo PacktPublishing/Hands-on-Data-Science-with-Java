eda33edf801ada1b0e8abb875ebc04d59b8c1950
rwangari <rwangari@rwanda.cmu.edu>
2019-01-25 16:07:44 +0200

    Created functions to implement Linear regression and tested the quality of the model with MAE

--- src/HouseLinearRegression.java
+++ src/HouseLinearRegression.java
@@ -15,18 +15,72 @@ public class HouseLinearRegression {
 		
 	}
 	
-	public  double[] predictingHousePrices (double[][] houseCharacteristis) {
+	/**
+	 * A function to predict the prices of the houses using the model we created
+	 * @param houseCharacteristis
+	 * @param theLinearModel
+	 * @return predicted Prices 
+	 */
+	public  static double[] predictingHousePrices (double[][] houseCharacteristis, OLS theLinearModel) {
 		
-		double [] myarr = {24,45,67};
+		double [] predictedPrices = new double [houseCharacteristis.length];
 		
-		return myarr;
-		
+		for(int i=0; i<houseCharacteristis.length;i++) {
+			
+			double []  oneHseCharacteristics= new double[houseCharacteristis[i].length];
+						
+			for(int j =0; j<houseCharacteristis[i].length; j++) {
+				oneHseCharacteristics[j]=houseCharacteristis[i][j];
+			}
+			
+			//Predicting
+			double predictedaVal = theLinearModel.predict(oneHseCharacteristics);
+			predictedPrices[i]=predictedaVal;
+			
+		}
+		return predictedPrices;
 	}
+	
+	/*
+	 * Getting the absolute errors but subtracting predicted values from actual Values
+	 * @param double[] actualValues
+	 * @param double[] predictedValues
+	 * @return double [] theAbsoluteError
+	 */
+	
+	public static double[] calculateAbsoluteError (double [] actualValues, double []  predictedValues) {
+		
+		double [] theAbsoluteError  = new double [actualValues.length];
+		
+		for(int k = 0; k<actualValues.length; k++) {
+			
+			double j =actualValues[k]-predictedValues[k];
+			
+			//putting the absolute error in the array			
+			theAbsoluteError[k]=Math.abs(j);		
+		}
+		return theAbsoluteError;
+	}
+	/*
+	 * Getting the Mean of the Absolute Errors
+	 * @param double [] theAbsoluteError
+	 * @return double ,Mean
+	 */
+	public static double getMAE(double [] theAbsoluteError) {
+		double totalSum=0;
+		double theMean;
+		for(int i =0; i<theAbsoluteError.length; i++) {
+			totalSum=totalSum+theAbsoluteError[i];
+		}
+		theMean=totalSum/theAbsoluteError.length;
+		return theMean;		
+	}
+	
 
 	public static void main(String[] args) throws IOException {
 	
 	/*
-	 * New strategy
+	 * Predicting the House prices using Linear Regression (Supervised Learning)
 	 */
 	
 	Table AllPricesHouse  = Table.read().csv("DataSets/HousePricesAll.csv");
@@ -113,8 +167,28 @@ public class HouseLinearRegression {
 		System.out.println("The actual value is 194000");
 		
 	
-
-	}
+/**
+ * Using the function to Predict the Values of our testing dataset
+ */
 		
+	//converting out test independent table to double array
+		double [][] AllPricesHouseTestIndependentArr =AllPricesHouseTestIndependent.as().doubleMatrix();
+	
+   //Calling the function
+		double []  predictedVals = predictingHousePrices(AllPricesHouseTestIndependentArr,myHousePriceTrainer);
+		System.out.println("Like we are done here" +predictedVals.length);
+		
+		
+/**
+ * Calculating the Mean Average error
+ */
+		double [] ActualHousePrices = AllPricesHouseTestDependent.asDoubleArray();
+		//Getting the MAE 
+		double theMae =getMAE(calculateAbsoluteError(ActualHousePrices,predictedVals));		
+		System.out.println("The MAE of the House Linear Regression Is" + theMae );
+		System.out.println("This means that our model is off by average of"+ theMae);
+					
+	}
+		 
 
 }
