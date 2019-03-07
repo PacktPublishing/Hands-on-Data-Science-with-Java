bce2e28a673fde09dd40dc1272ca71abe6c5bd15
rwangari <rwangari@rwanda.cmu.edu>
2019-03-07 22:45:50 +0200

    Added a class for a mapper and a reducer class to use in Hadoop

---
+++ src/HadoopMapper.java
@@ -0,0 +1,13 @@
+
+public class HadoopMapper {
+
+	public HadoopMapper() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/HadoopReducer.java
@@ -0,0 +1,13 @@
+
+public class HadoopReducer {
+
+	public HadoopReducer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
