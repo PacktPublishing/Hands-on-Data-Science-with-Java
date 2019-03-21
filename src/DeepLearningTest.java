6e48838efa2eec95235562f5aa742c85aa6e4da3
rwangari <rwangari@rwanda.cmu.edu>
2019-03-21 22:29:44 +0200

    Loaded data  for deep learning, added datavec dependencies and ND4J dependencies

--- src/DeepLearningTest.java
+++ src/DeepLearningTest.java
@@ -1,13 +1,57 @@
 
+import java.io.IOException;
+
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.io.ClassPathResource;
+//import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+/**
+ * Section 5 
+ * Creating a deeplearning Model using DeePLearnign 4J
+ * @author rwangari
+ *
+ */
+
 public class DeepLearningTest {
 
 	public DeepLearningTest() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/**
+		 * Loading the Dataset using Record Reader. CSV reader handles Loading and parsing of the data
+		 */
+        int numLinesToSkip = 0;
+        char delimiter = ',';
+        RecordReader irisRecordReader = new CSVRecordReader(numLinesToSkip,delimiter);
+        try {
+        	irisRecordReader.initialize(new FileSplit(new ClassPathResource("IrisFlowerData.txt").getFile()));
+		} catch (IOException e) {
+			System.out.println("We couldnt load the file because of FILE IO");
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			System.out.println("The file could not be loaded because of interupted Exeception");			
+			e.printStackTrace();
+		}
+        System.out.println("We have loaded the file");
+        
+    /**
+     * Defining the number of x variables, the number of classes in the flowers data and the total number of flowers in the file
+     */
+        int numExplanatoryVariables = 4; //Sepal length and width, petal Length and width
+        int numFlowerClasses =3; //Setosa, Virginica & Versicolor
+        int flowerBatchSize = 150; //Our iris dataset contains 150 samples 
+        
+        
+  /**
+   *  Using RecordReaderDataSetIterator to handle conversion  of our dataset to objects,
+   *  ready for use in neural network
+   */ 
+        DataSetIterator iterator = new RecordReaderDataSetIterator(irisRecordReader,flowerBatchSize,numExplanatoryVariables,numFlowerClasses);
+       // DataSet allData = iterator.next();
+       // allData.shuffle();
 
 	}
 
