6f95431fd1838d45b0c3192bf033c46343f7cfb1
rwangari <rwangari@rwanda.cmu.edu>
2019-03-24 21:22:57 +0200

    Added  Java code to split test and train dataset, build the model and evaluated the model. Also changed the iris dataset and included values 0,1 and to represent the different flowers instead of having their names in the dataset

--- src/DeepLearningTest.java
+++ src/DeepLearningTest.java
@@ -5,13 +5,25 @@ import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+//import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
 import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
 import org.nd4j.linalg.io.ClassPathResource;
+import org.nd4j.linalg.learning.config.Sgd;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 /**
  * Section 5 
  * Creating a deeplearning Model using DeePLearnign 4J
@@ -78,6 +90,41 @@ public class DeepLearningTest {
         final int numInputs = 4;
         int outputNum = 3;
         long seed = 6;
+        
+        System.out.println("Building the model,");
+        MultiLayerConfiguration layerConfiguration = new NeuralNetConfiguration.Builder()
+            .seed(seed)
+            .activation(Activation.TANH)
+            .weightInit(WeightInit.XAVIER)
+            .updater(new Sgd(0.1))
+            .l2(1e-4)
+            .list()
+            .layer(0, new DenseLayer.Builder().nIn(numInputs).nOut(3)
+                .build())
+            .layer(1, new DenseLayer.Builder().nIn(3).nOut(3)
+                .build())
+            .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .activation(Activation.SOFTMAX)
+                .nIn(3).nOut(outputNum).build())
+            .backprop(true).pretrain(false)
+            .build();
+        
+        //run the model
+        MultiLayerNetwork model = new MultiLayerNetwork(layerConfiguration);
+        model.init();
+        model.setListeners(new ScoreIterationListener(100));
+
+        for(int i=0; i<1000; i++ ) {
+            model.fit(irisTrainingData);
+        }
+        
+        
+        
+      //evaluate the model on the test set
+        Evaluation eval = new Evaluation(3);
+        INDArray output = model.output(irisTestData.getFeatures());
+        eval.eval(irisTestData.getLabels(), output);
+        System.out.println(eval.stats());
         
         
         
