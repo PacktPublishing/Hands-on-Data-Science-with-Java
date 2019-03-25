83e7ae2a328a00f4661b38f3a45a094cf6ade92e
rwangari <rwangari@rwanda.cmu.edu>
2019-03-25 17:23:29 +0200

    Implemented the sigmoid function, softmax function and other helping functions to be used in the deeplearning

--- src/DeepLearningNoFramework.java
+++ src/DeepLearningNoFramework.java
@@ -1,7 +1,6 @@
 
 import java.lang.Math.*;
 
-
 public class DeepLearningNoFramework {
 
 	public DeepLearningNoFramework() {
@@ -26,11 +25,71 @@ public class DeepLearningNoFramework {
 	
 	    return ret;
 	}
+	/**
+	 * Getting the summation of array contents
+	 * @param myArray
+	 * @return
+	 */
+	public static double getArraySummation(double[] myArray) {
+		double thesum= 0;
+		for (int i =0; i<myArray.length; i++) {
+			thesum=thesum+myArray[i];
+		}
+		return thesum;
+	}
 
+	/**
+	 * Defining the Sigmoid function
+	 * @param x
+	 * @return
+	 */
+	public static double[][] theSigmoid (double [][] x){
+		double exponents [][] = new double [x.length][x[0].length];
+		
+		for(int i= 0; i<x.length; i++) {
+			for (int k =0; k<x[i].length;k++) {
+				double val= Math.exp(-(x[i][k]));
+				double sigmoidVal = 1/(1+val);
+				exponents[i][k]=sigmoidVal;
+			}
+		}		
+		return exponents;
+	}
 	
-
+	/**
+	 * Defining the Softmax Function
+	 * @param A
+	 */	
+	public static double[] theSoftmax (double [] x){
+		double exponents [] = new double [x.length];
+		
+		for(int i= 0; i<x.length; i++) {
+				double expVal= Math.exp((x[i]));
+				
+				//double sigmoidVal = 1/(1+val);
+				exponents[i]=expVal;
+		}
+		
+		//Calculating the exponential divide by the sum
+		double softmaxExponents [] = new double [exponents.length];
+		for(int j =0; j<exponents.length; j++) {
+			
+			double myVal = exponents[j]/getArraySummation(exponents);
+			softmaxExponents[j]=myVal;
+			
+		}
+		
+		return softmaxExponents;
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+//		# generate three Gaussian clouds each holding 500 points
+//		X1 = np.random.randn(500, 2) + np.array([0, -2])
+//		X2 = np.random.randn(500, 2) + np.array([2, 2])
+//		X3 = np.random.randn(500, 2) + np.array([-2, 2])
 	
 		System.out.println("Processing the deeplearnign with no framework ");
 	}
